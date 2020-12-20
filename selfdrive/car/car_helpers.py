81170410967120c38e25281291cdf97617221a6c
stonerains <72440538+stonerains@users.noreply.github.com>
2020-12-20 15:26:22 +0900

    Update car_helpers.py
    스팅어 강제인식

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -177,7 +177,7 @@ def get_car(logcan, sendcan, has_relay=False):
 
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
-    candidate = "mock"
+    candidate = "KIA STINGER GT2 2018"
 
   CarInterface, CarController, CarState = interfaces[candidate]
   car_params = CarInterface.get_params(candidate, fingerprints, has_relay, car_fw)
